d3254e22be3afb3ec91683d4b12b92725afe6e23
jossiemiliza <90527041+jossiemiliza@users.noreply.github.com>
2021-09-13 12:18:16 +0300

    Delete backend directory

--- backend/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-// create an object of the imported package
-const app = express();
-//create web server
-app.listen(5000, console.log("Server started on port 5000"));
